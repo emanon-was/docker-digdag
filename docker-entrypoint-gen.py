8eb4446b2975b3e5c8f3225a55a5043790eb835b
Daigo Kawasaki <kawasaki@fujisan.co.jp>
2020-08-24 16:47:28 +0900

    Python/Jinja2を使ってtomlからdocker-entrypoint.shを書き出すスクリプトを作成

--- docker-entrypoint-gen.py
+++ docker-entrypoint-gen.py
@@ -1,2 +1,7 @@
+from jinja2 import Template, Environment, FileSystemLoader
 import toml
 
+template_env = Environment(loader=FileSystemLoader('templates'))
+template = template_env.get_template('docker-entrypoint.sh.j2')
+digdag_server_config = toml.load(open('digdag-server.toml'))
+print(template.render(conf=digdag_server_config))
